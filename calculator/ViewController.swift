30a1e7cbfd2e4f5c38dafad3be3a1e3822e95590
Brian <brian.irons@icloud.com>
2019-10-18 11:17:32 +0200

    first commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Brian Irons on 2019/10/18.
+//  Copyright © 2019 Brian Irons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
